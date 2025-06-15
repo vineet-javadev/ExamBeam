cc566244ceb43094e4e9983d41412ebed6030e02
vineet-javadev <vineetkumar444777@gmail.com>
2025-06-16 01:35:56 +0530

    v1.1

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  MISTRAL_API_KEY: process.env['MISTRAL_API_KEY'] || ''
-};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+// Mistral being a free large language model and that the key is not needed to be copied but can be obtained from the official website.
+
+export const environment = {
+  MISTRAL_API_KEY: 'Akj3NaTVu1FbXHQtC3LmX13H8y8LOGvI'
+};
